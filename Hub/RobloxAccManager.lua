a1aaf568b8509849978da260134f85cb47394ac7
ReQiuYTPL <56265894+ReQiuYTPL@users.noreply.github.com>
2023-01-05 17:19:17 +0100

    Add files via upload

---
+++ Hub/RobloxAccManager.lua
@@ -0,0 +1,19 @@
+repeat task.wait() until game:IsLoaded() and Nexus
+
+if not Nexus.IsConnected then Nexus.Connected:Wait() end
+
+Nexus:CreateButton('highland', 'Highland Cow', { 100, 20 }, { 10, 10, 10, 10 })
+Nexus:OnButtonClick('highland', function()
+    ReplicatedStorage.API:FindFirstChild("ShopAPI/BuyItem"):InvokeServer("pets","scottish_2023_highland_cow",{})
+end)
+
+Nexus:CreateTextBox('cuenta', 'Cuenta', { 100, 20 }, { 10, 10, 10, 10 })
+
+Nexus:CreateButton('dartodo', 'Dar todo', { 100, 20 }, { 10, 10, 10, 10 })
+Nexus:OnButtonClick('dartodo', function()
+    local MainAccount = Nexus:GetText('cuenta')
+    local Pets = require(game.ReplicatedStorage.ClientModules.Core.ClientData).get_data()[game.Players.LocalPlayer.Name].inventory.pets or {}
+    for k,v in pairs(Pets) do
+        game:GetService("ReplicatedStorage").API:FindFirstChild("TradeAPI/GiveItem"):InvokeServer(game:GetService("Players")[MainAccount], v['unique'])
+    end
+end)
