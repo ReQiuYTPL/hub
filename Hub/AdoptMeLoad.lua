2ab20abf1e46e3bac4c11d600908deeef3e50958
ReQiuYTPL <56265894+ReQiuYTPL@users.noreply.github.com>
2023-01-14 13:26:42 +0100

    Update AdoptMeLoad.lua

--- Hub/AdoptMeLoad.lua
+++ Hub/AdoptMeLoad.lua
@@ -1,10 +1,6 @@
 loadstring(game:HttpGet(('https://gitfront.io/r/ReQiuYTPL/wFUydaK74uGx/hub/raw/Hub/AdoptMe.lua'),true))()
 
-print(_G.LoadedPrices)
-
 while not _G.LoadedPrices do wait(1) print(_G.LoadedPrices) end
-
-print(_G.LoadedPrices)
 
 local mod = require(game:GetService("ReplicatedStorage").ClientModules.Core.UIManager.Apps.TradeApp)
 newFunc = hookfunction(mod._get_partner_offer, function(...)
